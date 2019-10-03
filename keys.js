d2ff2be67975922a6aa0544c02603106f2838e09
Madeleine Prak <madeleineprak@gmail.com>
2019-10-02 20:47:18 -0700

    Make omdb and bandsintown keys private, make data arrays for api responses to clean up

--- keys.js
+++ keys.js
@@ -2,3 +2,11 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+exports.bandsintown = {
+  id: process.env.BANDSINTOWN_KEY,
+};
+
+exports.omdb = {
+  id: process.env.OMDB_KEY,
+};
--- liri.js
+++ liri.js
@@ -14,35 +14,32 @@ var userInput = process.argv.slice(3).join(" ");
 // Renders when "concert-this" command is called to get the BandsInTown API
 function getBand(input) {
     // If no user input
-    if(!input) {
+    if (!input) {
         input = "Slander";
     }
-    var queryUrl = "https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp";
+    var queryUrl = "https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=" + keys.bandsintown.id;
     axios.get(queryUrl).then(
-        function(response) {
+        function (response) {
             var events = response.data;
-            
             console.log("UPCOMING EVENTS");
             console.log("==================");
-            // Returns all given events
-            // for (i=0; i<events.length; i++) {
 
             // Only returns first 5 events
-            for (i=0; i<5; i++) {
+            for (i = 0; i < 5; i++) {
                 var venue = events[i].venue.name;
                 var location = events[i].venue.city + ", " + events[i].venue.country;
                 var time = events[i].datetime;
                 time = time.substring(0, time.indexOf("T"));
                 var convertedTime = moment(time, 'YYYY-MM-DD');
                 convertedTime = convertedTime.format('MM/DD/YYYY');
-                
+
                 console.log("Venue: " + venue);
                 console.log("Location: " + location);
                 console.log("Date: " + convertedTime);
                 console.log(" ");
             }
         }
-    ).catch(function(error) {
+    ).catch(function (error) {
         console.log(error);
     });
 }
@@ -50,103 +47,68 @@ function getBand(input) {
 // Renders when "spotify-this-song" command is called to get the Spotify API
 function getSpotify(input) {
     // If no user input
-    if(!input) {
+    if (!input) {
         input = "The Sign by Ace of Base";
     }
     spotify
-        .search({ type: 'track', query: userInput, limit: 1})
-        .then(function(response) {
-
+        .search({ type: 'track', query: userInput, limit: 1 })
+        .then(function (response) {
             var song = response.tracks.items[0];
-            var artists = song.artists;
 
-            console.log("ARTIST(S)");
-            console.log("=============");
-            for(i=0; i<artists.length; i++) {
-                console.log(artists[i].name);
+            // Store artists in an array
+            var artistArray = [];
+            for (i = 0; i < song.artists.length; i++) {
+                artistArray.push(song.artists[i].name);
             }
-            console.log(" ");
 
-            console.log("SONG NAME");
-            console.log("=============");
-            console.log(song.name);
-            console.log(" ");
+            // Store response data in an array  
+            var dataArray = [
+                "Name: " + song.name,
+                "Artist(s): " + artistArray.join(", "),
+                "Link: " + song.external_urls.spotify,
+                "Album: " + song.album.name
+            ].join("\n\n");
 
-            console.log("SONG LINK");
-            console.log("=============");
-            console.log(song.external_urls.spotify);
-            console.log(" ");
-
-            console.log("ALBUM NAME");
-            console.log("=============");
-            console.log(song.album.name);
-
+            console.log(dataArray);
         })
-        .catch(function(err) {
+        .catch(function (err) {
             console.log(err);
         });
 }
 
 // Renders when "movie-this" command is called to get the OMDb API
 function getMovie(input) {
-    if(!input) {
+    if (!input) {
         input = "Mr. Nobody";
     }
-    var queryUrl = "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy";
+    var queryUrl = "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=" + keys.omdb.id;
     axios.get(queryUrl).then(
-        function(response) {
-        var movie = response.data;
+        function (response) {
+            var movie = response.data;
 
-        console.log("NAME");
-        console.log("=============");
-        console.log(movie.Title);
-        console.log(" ");
+            // Store response data in an array
+            var dataArray = [
+                "Name: " + movie.Title,
+                "Year Released: " + movie.Year,
+                "IMDb Rating: " + movie.imdbRating,
+                "Rotten Tomatoes Rating: " + movie.Ratings[1].Value,
+                "Country Produced: " + movie.Country,
+                "Language: " + movie.Language,
+                "Plot: " + movie.Plot,
+                "Actors: " + movie.Actors
+            ].join("\n\n");
 
-        console.log("YEAR RELEASED");
-        console.log("=============");
-        console.log(movie.Year);
-        console.log(" ");
-
-        console.log("IMDB RATING");
-        console.log("=============");
-        console.log(movie.imdbRating);
-        console.log(" ");
-
-        console.log("ROTTEN TOMATOES RATING");
-        console.log("=============");
-        console.log(movie.Ratings[1].Value);
-        console.log(" ");
-
-        console.log("COUNTRY PRODUCED");
-        console.log("=============");
-        console.log(movie.Country);
-        console.log(" ");
-
-        console.log("LANGUAGE");
-        console.log("=============");
-        console.log(movie.Language);
-        console.log(" ");
-
-        console.log("PLOT");
-        console.log("=============");
-        console.log(movie.Plot);
-        console.log(" ");
-
-        console.log("ACTORS");
-        console.log("=============");
-        console.log(movie.Actors);
-        console.log(" ");
-        
+            console.log(dataArray);
         })
-        .catch(function(error) {
-          console.log(error);
+        .catch(function (error) {
+            console.log(error);
         });
 }
 
 // Chooses which function to run based on user input command
 function runLiri(input) {
     switch (userCommand) {
-        case "concert-this": 
+        case "concert-this":
             getBand(input);
             break;
         case "spotify-this-song":
@@ -155,7 +117,7 @@ function runLiri(input) {
         case "movie-this":
             getMovie(input);
             break;
-        case "do-what-it-says": 
+        case "do-what-it-says":
             Random();
             break;
     }
@@ -163,7 +125,7 @@ function runLiri(input) {
 
 // Renders when "do-what-it-says" command is run and reads the random.txt file
 function Random() {
-    fs.readFile("random.txt", "utf8", function(error, data) {
+    fs.readFile("random.txt", "utf8", function (error, data) {
         if (error) {
             return console.log(error);
         }
@@ -174,5 +136,6 @@ function Random() {
     })
 }
 
+// Initial run
 runLiri(userInput);
 
